5f419a2c7147914210e290470754af3d50cae02f
bazz-00 <baziukkostia@gmail.com>
2022-06-16 18:55:47 +0300

    add postgree

--- shuba/shuba/settings.py
+++ shuba/shuba/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'shuba.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': 'shuba',
+'USER': 'shuba',
+'PASSWORD': '5555165',
+'HOST': 'localhost',
+'PORT': '5432'
+}
 }
 
 
