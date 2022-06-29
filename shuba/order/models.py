fb2f1897e91f4f3ada26f505d74865a83c8d2f60
bazz-00 <baziukkostia@gmail.com>
2022-06-29 21:16:53 +0300

    add Lesha

--- shuba/order/models.py
+++ shuba/order/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     city = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
     phoneNumber = models.CharField(validators=[phoneNumberRegex], max_length=16, unique=True)
-    #photo = models.FileField(upload_to='uploads/%Y/%m/%d/', blank=True)
+    # photo = models.FileField(upload_to='uploads/%Y/%m/%d/', blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     date_finish = models.DateField(null=True, blank=True)
@@ -51,7 +51,7 @@ class OrderPhotos(models.Model):
         db_table = 'orderphotos'
 
     def __str__(self):
-        return self.order
+        return str(self.order)
 
 
 
