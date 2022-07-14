5735866936a60ce62558c6f028d2266dab4fb108
Alekseyshutko <Aleksey_shutko@rambler.ru>
2022-07-15 01:11:55 +0300

    add in order filtr category

--- shuba/order/api.py
+++ shuba/order/api.py
@@ -1,12 +1,17 @@
 from .models import Order, OrderComments, OrderPhotos
 from rest_framework import viewsets, permissions
 from .serializers import OrderSerializer, OrderCommentsSerializer, OrderPhotosSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = OrderSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category']
+
+
 
 
 class OrderCommentsViewSet(viewsets.ModelViewSet):
--- shuba/order/serializers.py
+++ shuba/order/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import Order, OrderComments, OrderPhotos
 
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
