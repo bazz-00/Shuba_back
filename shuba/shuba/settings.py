f81955096b064c62772160c7257b6f9fe13b89e1
bazz-00 <baziukkostia@gmail.com>
2022-07-30 12:48:57 +0300

    heroku

--- shuba/shuba/settings.py
+++ shuba/shuba/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2ut-xnu7co+9yad5543%mbx97he@%s@y_m1sx)8nfqh882rg6r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
